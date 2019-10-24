dce7e57f4ac449151454692047bd74a38aee4e66
stefan.doyscher@virotechdiagnostics.com <sdoyschman>
2019-10-24 17:07:11 +0200

    Splash Screen - Vorbereitung als Login Form, die vor den anderen Formularen geladen werden soll

--- CentralGUI.dpr
+++ CentralGUI.dpr
@@ -2,13 +2,15 @@ program CentralGUI;
 
 uses
   Vcl.Forms,
-  u_main in 'src\u_main.pas' {MainForm};
+  u_main in 'src\u_main.pas' {MainForm},
+  frm_splash in 'src\frm_splash.pas' {frm_Login};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TMainForm, MainForm);
+  Application.CreateForm(TSplashForm, frm_Login);
+  {Application.CreateForm(TMainForm, MainForm);}
   Application.Run;
 end.
