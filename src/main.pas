579a85b9d123776483244c3ebb4e147f87017925
didgeman <stefan@doyscher.de>
2020-02-11 22:12:57 +0100

    label in upper right corner now displays fps

--- src/main.pas
+++ src/main.pas
@@ -22,11 +22,15 @@ type
     MenuPanel: TPanel;
     StatusPanel: TPanel;
     GameViewPort: TRectangle;
+    chk_IsDoubleBuffered: TCheckBox;
+    lblFPS_Counter: TLabel;
     procedure OnGameLoopTick(Sender: TObject);
     procedure FormKeyDown(Sender: TObject; var Key: Word; var KeyChar: Char;
       Shift: TShiftState);
     procedure FormKeyUp(Sender: TObject; var Key: Word; var KeyChar: Char;
       Shift: TShiftState);
+    procedure chk_IsDoubleBufferedChange(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
     { Private-Deklarationen }
   public
@@ -36,6 +40,8 @@ type
 var
   MainForm: TMainForm;
   currGameInput: SGameInput;
+  fps: Double;
+  lastTime, currentTime, elapsedTime: System.Cardinal;
 
 { module procs and functions since they are app-wide unique anyway }
 { if they should be obj-bound, a kind of Main-Class-Obj have to be invented }
@@ -53,6 +59,7 @@ var
 procedure startGame();
 begin
   IsGameRunning := True;
+  lastTime := TThread.GetTickCount;
 end;
 
 procedure stopGame();
@@ -62,6 +69,18 @@ end;
 
 
 {--- START  Win event handlers  ---}
+
+procedure TMainForm.chk_IsDoubleBufferedChange(Sender: TObject);
+begin
+  {if chk_IsDoubleBuffered.IsChecked then
+    DoubleBuffered := True
+  else}
+end;
+
+procedure TMainForm.FormCreate(Sender: TObject);
+begin
+  lastTime := TThread.GetTickCount;
+end;
 
 procedure TMainForm.FormKeyDown(Sender: TObject; var Key: Word;
   var KeyChar: Char; Shift: TShiftState);
@@ -105,7 +124,12 @@ end;
 procedure TMainForm.OnGameLoopTick(Sender: TObject);
 begin
   if IsGameRunning then begin
-
+    currentTime := TThread.GetTickCount;
+    elapsedTime := currentTime - lastTime;    { will be used for update() }
+    fps := (1000.0 / Double(elapsedTime));
+    lastTime := currentTime;
+    GameViewPort.BeginUpdate;
+    lblFPS_Counter.Text := fps.ToString;
     { process the Input-Info for player actions }
     if (currGameInput.LeftButtonPressed = True)
         {and (gameobj1.Position.X - 2 >= ClientLayout.Position.X)} then
@@ -118,15 +142,16 @@ begin
      { gameobj1.Position.X := gameobj1.Position.X + 2;}
       gameobj1.RotationAngle := gameobj1.RotationAngle + 2.0;
     if (currGameInput.UpButtonPressed) then
-      gameobj1.Position.Y := gameobj1.Position.Y - 2.0;
+      gameobj1.Position.Y := gameobj1.Position.Y - (elapsedTime*200/1000);
     if (currGameInput.DownButtonPressed) then
-      gameobj1.Position.Y := gameobj1.Position.Y + 2.0;
+      gameobj1.Position.Y := gameobj1.Position.Y + (elapsedTime*200/1000);
 
 
     { change state because of game rules, physics... }
 
 
     { render ?? implicit }
+    GameViewPort.EndUpdate;
   end;
 
 end;
